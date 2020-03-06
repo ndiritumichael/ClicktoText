dce95a1f33e6c3042abe11dcc9c40b75eb797478
ndiritu_michael <ndiritumichael5@gmail.com>
2020-03-06 16:55:43 +0300

    used country code picker library

--- app/src/main/java/com/keapps/textNumber/MainActivity.kt
+++ app/src/main/java/com/keapps/textNumber/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.keapps.textNumber
 
-import android.content.Context
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.hbb20.CountryCodePicker
+import java.net.URLEncoder
 
 
 class MainActivity : AppCompatActivity() {
@@ -15,27 +17,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var editText = findViewById<EditText>(R.id.inputText)
+        var number = findViewById<EditText>(R.id.number_tv)
         var sendButton = findViewById<Button>(R.id.buttonSend)
-        var editText1 = findViewById<EditText>(R.id.messageTV).toString()
-        var msgTxt = Uri.encode(editText1)
-        var x = 254718428473
-
-
-
-
-
+        var editText1 = findViewById<EditText>(R.id.messageTV)
+        var x = number.text
+        var cpp = findViewById<CountryCodePicker>(R.id.ccp)
+        cpp.registerCarrierNumberEditText(number)
         sendButton.setOnClickListener {
-
-
+            var fullnumber = cpp.fullNumber
+            var msgTxt = editText1.text.toString()
+            var query1 = URLEncoder.encode(msgTxt, "utf-8")
             val LINK = "https://wa.me/"
-            var numtext = x.toString()
             var link: String
-            /* if (msgTxt != null)
-            { link = LINK+numtext+msgTxt} else*/
-            link = LINK + numtext + "?" + msgTxt
-
-
+            link = LINK + fullnumber + "?text=" + query1
+            Toast.makeText(this, link + "", Toast.LENGTH_LONG).show()
             var browseIntent = Intent(Intent.ACTION_VIEW, Uri.parse(link))
             startActivity(browseIntent)
         }
